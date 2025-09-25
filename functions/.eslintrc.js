2244f6aaeb090435a1f42e731e27e5810d1b0b23
PhonePyaeMyat <phonepyae1282002@gmail.com>
2025-09-25 13:20:22 +0100

    fix error version 2

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -7,11 +7,15 @@ module.exports = {
   extends: [
     'eslint:recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
   rules: {
     'no-restricted-globals': ['error', 'name', 'length'],
     'prefer-arrow-callback': 'error',
-    'quotes': ['error', 'single', {'allowTemplateLiterals': true}],
-    'max-len': ['error', {'code': 120}],
+    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    'max-len': ['error', { 'code': 120 }],
     'object-curly-spacing': ['error', 'always'],
     'linebreak-style': 'off', // Disable for Windows
   },
