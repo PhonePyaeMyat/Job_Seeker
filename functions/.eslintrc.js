8c7cfef2a598d352ca5c920b0638e4454c8b1313
PhonePyaeMyat <phonepyae1282002@gmail.com>
2025-09-25 13:12:25 +0100

    Fix firebase error

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "google",
+    'eslint:recommended',
   ],
   rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "single", {"allowTemplateLiterals": true}],
-    "max-len": ["error", {"code": 120}],
-    "object-curly-spacing": ["error", "always"],
+    'no-restricted-globals': ['error', 'name', 'length'],
+    'prefer-arrow-callback': 'error',
+    'quotes': ['error', 'single', {'allowTemplateLiterals': true}],
+    'max-len': ['error', {'code': 120}],
+    'object-curly-spacing': ['error', 'always'],
+    'linebreak-style': 'off', // Disable for Windows
   },
   overrides: [
     {
-      files: ["**/*.spec.*"],
+      files: ['**/*.spec.*'],
       env: {
         mocha: true,
       },
